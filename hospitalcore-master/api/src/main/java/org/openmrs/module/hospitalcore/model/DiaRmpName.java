9ec94857234898750c6934eba582e9dffe007b0a
Farzana <Farzana@Farzana>
2016-12-22 17:12:17 +0600

    Medisun>Registration+Billing> work in RMP Create, Update

--- hospitalcore-master/api/src/main/java/org/openmrs/module/hospitalcore/model/DiaRmpName.java
+++ hospitalcore-master/api/src/main/java/org/openmrs/module/hospitalcore/model/DiaRmpName.java
@@ -12,11 +12,17 @@ import java.util.Date;
  * @author Khairul
  */
 public class DiaRmpName {
-
+    
     public int id;
     public String name;
+    public String designation;
+    public String address;
+    public String phone;
+    public String degree;
+    public String marketed_by;
     public int creator;
     public Date createdDate;
+
 
     public int getId() {
         return id;
@@ -32,6 +38,46 @@ public class DiaRmpName {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getMarketed_by() {
+        return marketed_by;
+    }
+
+    public void setMarketed_by(String marketed_by) {
+        this.marketed_by = marketed_by;
     }
 
     public int getCreator() {
